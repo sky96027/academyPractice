727edd324ee75c1b2acd05457573384e22390012
kyg <sky960271@gmail.com>
2021-11-11 17:43:01 +0900

    Update ExbEqualsEx2.java

--- KYG/src/day12/ExbEqualsEx2.java
+++ KYG/src/day12/ExbEqualsEx2.java
@@ -4,6 +4,7 @@ public class ExbEqualsEx2 {
 
 	public static void main(String[] args) {
 		// 문자열 비교해보기
+		// 문자열을 초기화할 때
 		String str1 = "Hello";
 		String str2 = new String( "Hello" );
  		String str3 = "Hello";
@@ -19,6 +20,44 @@ public class ExbEqualsEx2 {
 		else {
 			System.out.println(" == : 두 문자열이 다르다");
 		}
+		
+		System.out.println();
+		
+		//equals와 비교
+		if(str1.equals(str2)) {
+			System.out.println(" == : 두 문자열이 같다");
+		}
+		else {
+			System.out.println(" == : 두 문자열이 다르다");
+		}
+		if(str1.equals(str3)) {
+			System.out.println(" == : 두 문자열이 같다");
+		}
+		else {
+			System.out.println(" == : 두 문자열이 다르다");
+		}
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
