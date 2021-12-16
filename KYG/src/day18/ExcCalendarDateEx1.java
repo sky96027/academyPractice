071af8893a80dc616ecc0d46559c16db3ebfe604
sky96027 <93393540+sky96027@users.noreply.github.com>
2021-12-16 09:28:46 +0900

    45

--- KYG/src/day18/ExcCalendarDateEx1.java
+++ KYG/src/day18/ExcCalendarDateEx1.java
@@ -6,16 +6,16 @@ import java.util.Date;
 public class ExcCalendarDateEx1 {
 
 	public static void main(String[] args) {
-		/* Calendar를 Date로 */
+		/* Calendar瑜� Date濡� */
 		Calendar cal = Calendar.getInstance();
 
 		Date date = new Date(cal.getTimeInMillis());
 		System.out.println(date);
 
-		/* Date를 Calendar로*/
+		/* Date瑜� Calendar濡�*/
 		Date date2 = new Date();
 		Calendar cal2 = Calendar.getInstance();
-		//date2가 Calendar로 바뀐걸 구분할 수 있게 cal2의 년도를 2022로 수정
+		//date2媛� Calendar濡� 諛붾�먭구 援щ텇�븷 �닔 �엳寃� cal2�쓽 �뀈�룄瑜� 2022濡� �닔�젙
 		cal2.set(Calendar.YEAR, 2022);
 		cal2.setTime(date2);
 		System.out.println(cal2);
